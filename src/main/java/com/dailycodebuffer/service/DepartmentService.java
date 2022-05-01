fd099a29f06c15a4ea97f593ad21e606dc206a95
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2022-05-01 22:14:58 +0200

    implement the get all departments

--- src/main/java/com/dailycodebuffer/service/DepartmentService.java
+++ src/main/java/com/dailycodebuffer/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.dailycodebuffer.service;
 
 import com.dailycodebuffer.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     Department saveDepartment(Department department);
+
+    List<Department> fetchDepartmentList();
 }
--- src/main/java/com/dailycodebuffer/service/DepartmentServiceImpl.java
+++ src/main/java/com/dailycodebuffer/service/DepartmentServiceImpl.java
@@ -5,11 +5,19 @@ import com.dailycodebuffer.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
 
     @Autowired
     private DepartmentRepository departmentRepository;
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
+    }
+
     @Override
     public Department saveDepartment(Department department) {
         return departmentRepository.save(department);
