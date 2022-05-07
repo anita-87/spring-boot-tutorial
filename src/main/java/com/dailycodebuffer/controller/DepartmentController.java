f267de1d6a811b6d18b2850253ec3f86dacbe36e
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2022-05-07 23:31:58 +0200

    adding loggers

--- src/main/java/com/dailycodebuffer/controller/DepartmentController.java
+++ src/main/java/com/dailycodebuffer/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.dailycodebuffer.controller;
 
 import com.dailycodebuffer.entity.Department;
 import com.dailycodebuffer.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -13,13 +15,17 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
