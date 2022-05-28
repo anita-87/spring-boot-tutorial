f359d905b0c97392e224fe6b53f9068170fa2b76
Ana Rodriguez Fernandez <ana.rodriguez.fernandez.87@gmail.com>
2022-05-28 20:11:18 +0200

    unit tests for the service layer

--- src/test/java/com/dailycodebuffer/SpringBootTutorialApplicationTests.java
+++ src/test/java/com/dailycodebuffer/SpringBootTutorialApplicationTests.java
@@ -1,13 +1,8 @@
 package com.dailycodebuffer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootTutorialApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
